d2436443847eb9833c9331d55050fbc305010b31
Byeon Seongun <kamyu@kamyu.me>
2022-02-27 03:35:02 +0900

    add press enter to exit

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/hypebeast/go-osc/osc"
-	"log"
 	"os"
 	"os/signal"
 	"time"
@@ -21,13 +20,19 @@ func init() {
 	ConfigLoader()
 }
 
+func ExitProgram(msg string) {
+	fmt.Println(msg)
+	fmt.Println("press 'enter' to exit this program")
+	_, _ = fmt.Scanln()
+	os.Exit(0)
+}
+
 func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	if config.WidgetId == "" {
-		fmt.Println("widget_id is empty!!")
-		os.Exit(0)
+		ExitProgram("widget_id is empty!!")
 		return
 	}
 
@@ -36,20 +41,22 @@ func main() {
 	timeout = time.NewTimer(0)
 	go WsTimeoutChecker()
 
-	webSocketUrl := GetWebSocketUrl(config.WidgetId)
-	fmt.Println("connecting to websocket server")
-	var err error
-	wsConn, _, err = websocket.DefaultDialer.Dial(webSocketUrl, nil)
+	url, err := GetWebSocketUrl(config.WidgetId)
 	if err != nil {
-		panic(err)
+		ExitProgram("an error occurred while getting websocket url!!")
+		return
 	}
-	defer wsConn.Close()
+
+	fmt.Println("connecting to stromno websocket server")
+	wsConn, _, err = websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		ExitProgram("stromno websocket server connection failed..")
+		return
+	}
+	fmt.Println("stromno websocket server connected!")
+	defer func() { _ = wsConn.Close() }()
 
 	go WsReceiver()
-	//go OscParameterReceiver(func(msg *osc.Message) {
-	//	SetConnectedValue(connected)
-	//	fmt.Println("avatar change detected")
-	//})
 
 	for {
 		select {
@@ -57,7 +64,7 @@ func main() {
 			SetConnectedValue(false)
 			return
 		case <-interrupt:
-			log.Println("interrupt")
+			SetConnectedValue(false)
 			return
 		}
 	}
