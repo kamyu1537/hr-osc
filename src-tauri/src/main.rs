f71a286a5101c6241a0a39af2b9e1e4f3b0d432d
Byeon Seongun <kamyu1537@gmail.com>
2022-10-16 22:04:11 +0900

    lazy_static 추가, UdpSocket 을 lazy_static 으로 한번만 생성하도록 변경

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,38 +3,42 @@
     windows_subsystem = "windows"
 )]
 
+#[macro_use]
+extern crate lazy_static;
+
 use std::net::{UdpSocket};
 use rosc::encoder;
 use rosc::{OscMessage, OscPacket, OscType};
 
+lazy_static! {
+    static ref SOCKET: UdpSocket = {
+        UdpSocket::bind("127.0.0.1:0").unwrap()
+    };
+}
+
 #[tauri::command]
 fn send_float(addr: &str, path: &str, value: f32) {
-    let sock = UdpSocket::bind("127.0.0.1:0").unwrap();
-
     let msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
         addr: path.to_string(),
         args: vec![OscType::Float(value)],
     })).unwrap();
 
-    sock.send_to(&msg_buf, addr).unwrap();
+    SOCKET.send_to(&msg_buf, addr).unwrap();
 }
 
 #[tauri::command]
 fn send_bool(addr: &str, path: &str, value: bool) {
-    let sock = UdpSocket::bind("127.0.0.1:0").unwrap();
-
     let msg_buf = encoder::encode(&OscPacket::Message(OscMessage {
         addr: path.to_string(),
         args: vec![OscType::Bool(value)],
     })).unwrap();
 
-    sock.send_to(&msg_buf, addr).unwrap();
+    SOCKET.send_to(&msg_buf, addr).unwrap();
 }
 
 #[tokio::main]
 async fn main() {
     tauri::Builder::default()
-        // .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![send_float, send_bool])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
