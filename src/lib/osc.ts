e7250383b6bbe43b5c52bf87f1ddd527c2bcb077
Byeon Seongun <kamyu1537@gmail.com>
2023-01-26 18:51:12 +0900

    fix: change sendOscBool use getAddr func

--- src/lib/osc.ts
+++ src/lib/osc.ts
@@ -3,7 +3,7 @@ import { IConfig } from './config';
 
 const getAddr = (config: IConfig) => {
   let host = config.osc_client_host;
-  if (new RegExp('(?:[0-9]{1,3}.){3}[0-9]{1,3}', 'g').test(host)) {
+  if (!new RegExp('(?:[0-9]{1,3}.){3}[0-9]{1,3}', 'g').test(host)) {
     host = '127.0.0.1';
   }
 
@@ -15,5 +15,5 @@ export const sendOscFloat = (config: IConfig, path: string, value: number) => {
 };
 
 export const sendOscBool = (config: IConfig, path: string, value: boolean) => {
-  invoke('send_bool', { addr: config.osc_client_host + ':' + config.osc_client_port, path, value });
+  invoke('send_bool', { addr: getAddr(config), path, value });
 };
