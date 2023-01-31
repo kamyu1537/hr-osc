4a8633126cec8fef0bd34341523e57983496d84d
Byeon Seongun <kamyu1537@gmail.com>
2023-01-31 16:46:10 +0900

    feat: add relaunch button

--- src/components/settings/Http.tsx
+++ src/components/settings/Http.tsx
@@ -1,30 +1,29 @@
+import { relaunch } from '@tauri-apps/api/process';
+import { useState } from 'react';
 import useInput from '../../hooks/useInput';
 import { defaultConfig, saveConfig } from '../../lib/config';
-import { startHttpServer } from '../../lib/http_server';
 import { useConfig } from '../../lib/states';
 
 const Http = () => {
   const config = useConfig((state) => state.config);
+  const [port, setPort] = useState(config?.http_server_port || 0);
 
-  const serverPort = useInput(
-    { placeholder: defaultConfig.http_server_port + '', type: 'number' },
-    config?.http_server_port,
-    (val) => saveConfig({ ...(config || defaultConfig), http_server_port: val })
-  );
+  const serverPort = useInput({ placeholder: defaultConfig.http_server_port + '', type: 'number' }, config?.http_server_port, (val) => saveConfig({ ...(config || defaultConfig), http_server_port: val }));
 
   return (
     <div className="flex flex-col gap-2">
       <div className="text-sm leading-3">HTTP Server Port:</div>
       {serverPort.component}
 
+      <div className="text-sm leading-3 text-gray-500">Apply the changed port, hr-osc must be relaunched.</div>
+
       <button
         className="bg-gray-800 hover:bg-gray-700 px-2 py-0.5 rounded-md hover:text-cyan-400 transition-colors"
         onClick={() => {
-          console.info('http server start');
-          startHttpServer(config || defaultConfig);
+          relaunch();
         }}
       >
-        Server Restart
+        Relaunch
       </button>
     </div>
   );
